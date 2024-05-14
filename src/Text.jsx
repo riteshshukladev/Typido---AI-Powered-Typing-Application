35b9fa5e09c5bc68395536c26487f4a4e5e7ee16
RItesh <shuklaritesh5678@gmail.com>
2024-05-15 02:10:11 +0530

    Added the toBeTyped state and currentTyping state and set with that both state and added logic to fill the data inside that state

--- src/Text.jsx
+++ src/Text.jsx
@@ -8,6 +8,10 @@ function TypingTest() {
   const [toBeTyped, setToBeTyped] = useState([]);
   const [typed, setTyped] = useState([]);
   const [currentTyping, setCurrentTyping] = useState([]);
+  const [indicators, setIndicators] = useState({
+    startIndex: 0,
+    endIndex: 4,
+  });
 
   useEffect(() => {
     function splitStringIntoGroups(string, wordsPerGroup = 8) {
@@ -21,32 +25,47 @@ function TypingTest() {
       return groups;
     }
 
-    setToBeTyped(splitStringIntoGroups(output));
-    setCurrentTyping(splitStringIntoGroups(output)[0]);
-  }, [output]);
+    // setToBeTyped(splitStringIntoGroups(output));
+    // setCurrentTyping(splitStringIntoGroups(output)[0]);
 
-  useEffect(() => {
-    toBeTyped.map((group, index) => {
-      console.log(`Group ${index + 1}: ${group}`);
-    });
+    let temp = splitStringIntoGroups(output);
+    console.log(temp);
+    let newToBeTyped = [...toBeTyped];
+    for (let i = indicators.startIndex; i < indicators.endIndex; i++) {
+      newToBeTyped.push(temp[i]);
+    }
+    setToBeTyped(newToBeTyped.slice(1));
 
-    console.log(toBeTyped);
-  }, [toBeTyped]);
+    setCurrentTyping(temp[indicators.startIndex]);
+  }, [output, indicators]);
 
-  return(
+
+  const handleBtnClick = () => {
+    let newIndicators = { ...indicators };
+    newIndicators.startIndex += 1;
+    newIndicators.endIndex += 1;
+    setIndicators(newIndicators);
+    setToBeTyped([]);
+    setCurrentTyping(toBeTyped[indicators.startIndex]);
+
+
+  }
+
+  return (
     <div>
-      <h2 style={{color:"blue"}}>{currentTyping}</h2>
+      <h2 style={{ color: "blue" }}>{currentTyping}</h2>
       <br />
-      {toBeTyped.map((val)=>{
-        return(
-          <div>
+      <button onClick={handleBtnClick}>clickme</button>
+      <br />
+      {toBeTyped.map((val , index) => {
+        return (
+          <div  index = {index} >
             <h2>{val}</h2>
           </div>
-        )
+        );
       })}
-      
     </div>
-  )
+  );
 }
 
 export default TypingTest;
